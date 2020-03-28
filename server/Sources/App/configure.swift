7100fc9586329214313194196d46a81c699b0584
Jeffrey Sulton <jsulton@me.com>
2020-03-27 18:51:55 -0700

    Update to use PostgreSQL
    * Update `configure` to  check for database environment variables
    * Update Package.swift
    * Update docker-compose

--- server/Sources/App/configure.swift
+++ server/Sources/App/configure.swift
@@ -1,5 +1,5 @@
 import Fluent
-import FluentSQLiteDriver
+import FluentPostgresDriver
 import Vapor
 
 // configures your application
@@ -7,7 +7,12 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.databases.use(.postgres(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .psql)
 
     app.migrations.add(CreateLists())
     app.migrations.add(CreateTask())
