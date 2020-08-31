fa8231d50365007aa9343cfecfec5d66dd4779f0
Jeffrey Sulton <jeffrey_sulton@bug.local>
2020-08-31 11:04:35 -0700

    Update unit test to test the health route

--- server/Tests/AppTests/AppTests.swift
+++ server/Tests/AppTests/AppTests.swift
@@ -2,14 +2,14 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() throws {
+    func testHealth() throws {
         let app = Application(.testing)
         defer { app.shutdown() }
         try configure(app)
 
-        try app.test(.GET, "hello") { res in
+        try app.test(.GET, "health") { res in
             XCTAssertEqual(res.status, .ok)
-            XCTAssertEqual(res.body.string, "Hello, world!")
+            XCTAssertEqual(res.body.string, "ok")
         }
     }
 }
