a20207c761c1e7961b6840617724a55a25b0de0c
Jeffrey Sulton <notlus68@me.com>
2022-01-07 22:28:50 -0800

    Add iOS target files

---
+++ YnTasksApp/iOS/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  YnTasks iOS
+//
+//  Created by Jeffrey Sulton on 1/3/22.
+//  Copyright © 2022 Jeffrey Sulton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var listModel = YnListsViewModel()
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            NavigationView {
+                HStack {
+                    YnListsView(listModel: listModel)
+                        .listStyle(SidebarListStyle())
+                }
+            }
+            .onAppear {
+                self.listModel.fetch()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YnTasksApp/iOS/YnTasks_iOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  YnTasks_iOSApp.swift
+//  YnTasks iOS
+//
+//  Created by Jeffrey Sulton on 1/3/22.
+//  Copyright © 2022 Jeffrey Sulton. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct YnTasks_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
